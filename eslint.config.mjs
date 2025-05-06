d9507886b8e2004dafc09c66daa31744c0b2fcf0
Bohdan Bulakh <bgdn.56t@gmail.com>
2025-05-07 01:38:02 +0300

    linter: added a plugin for removing of unused imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@
 import eslint from '@eslint/js';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import unusedImports from "eslint-plugin-unused-imports";
 
 export default tseslint.config(
   {
@@ -24,6 +25,9 @@ export default tseslint.config(
     },
   },
   {
+    plugins: {
+      "unused-imports": unusedImports,
+    },
     rules: {
       '@typescript-eslint/no-unsafe-return': 'off',
       '@typescript-eslint/no-unsafe-assignment': 'off',
@@ -32,6 +36,17 @@ export default tseslint.config(
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
+      "no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          "vars": "all",
+          "varsIgnorePattern": "^_",
+          "args": "after-used",
+          "argsIgnorePattern": "^_"
+        }
+      ]
     },
   },
 );
