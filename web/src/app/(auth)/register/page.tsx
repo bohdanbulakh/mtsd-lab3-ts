2050788a8b075509d1a69a147272a856b763c3ff
Bohdan Bulakh <bgdn.56t@gmail.com>
2025-05-08 22:12:46 +0300

    feature: created registration form

---
+++ web/src/app/(auth)/register/page.tsx
@@ -0,0 +1,9 @@
+import RegisterForm from '@/app/(auth)/register/components/RegisterForm';
+
+export default function Register () {
+  return (
+    <div className="mt-10">
+      <RegisterForm/>
+    </div>
+  );
+}
