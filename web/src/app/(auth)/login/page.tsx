11961556a47c5822ecb31b802f627c45ad07b156
Bohdan Bulakh <bgdn.56t@gmail.com>
2025-05-08 17:47:57 +0300

    feature: created a login form

---
+++ web/src/app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+import LoginForm from '@/app/(auth)/login/components/LoginForm';
+
+export default function Login () {
+  return (
+    <div className="mt-10">
+      <LoginForm/>
+    </div>
+  );
+}
