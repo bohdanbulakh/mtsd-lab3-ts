9500cd75fc49d2978c18b35aec50fa827d2414bd
Bohdan Bulakh <bgdn.56t@gmail.com>
2025-05-10 15:54:44 +0300

    enhancement: updated header

--- web/src/components/layout/header/Header.tsx
+++ web/src/components/layout/header/Header.tsx
@@ -9,20 +9,35 @@ import {
   NavigationMenuList,
   navigationMenuTriggerStyle,
 } from '@/components/ui/navigation-menu';
+import Image from 'next/image';
+import { Label } from '@/components/ui/label';
 
 export default function Header () {
   return (
-    <NavigationMenu className="h-20 max-w-screen">
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <NavigationMenuLink
-            href="/login"
-            className={navigationMenuTriggerStyle()}
-          >
-            User Info
-          </NavigationMenuLink>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
+    <header className="flex items-center justify-between w-full px-6 bg-white shadow-md sticky top-0 z-50">
+      <div className="relative w-[100px] h-[48px] sm:w-[150px] sm:h-[64px] md:w-[250px] md:h-[80px] lg:w-[300px] lg:h-[96px] min-w-[100px] min-h-[48px]">
+        <Image
+          src="/logo.png"
+          alt="Stone Store Logo"
+          fill
+          priority
+          className="object-contain"
+        />
+      </div>
+      <NavigationMenu className="mr-[3%]">
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              href="/login"
+              className={navigationMenuTriggerStyle()}
+            >
+              <Label className="font-bold text-base sm:text-xl md:text-2xl lg:text-3xl">
+                Account
+              </Label>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </header>
   );
 }
